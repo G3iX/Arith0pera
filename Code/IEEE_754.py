4ffb21d9538ae89763eaa8d855528259a089ea51
Serjo <savenkoserhii@knu.ua>
2023-03-27 14:38:16 +0300

    beta 0.4.4 - IEEE_754 test # 3, added negative

--- Code/IEEE_754.py
+++ Code/IEEE_754.py
@@ -4,19 +4,21 @@
 # Normalize result
 import math
 
-from  BinaryCalcFunc import binary_format
-from  BinaryCalcFunc import binary_array_sum
+
 def iee_addition(input_num_x, input_num_y):
     if not isinstance(input_num_x, float) or not isinstance(input_num_y, float): #or max(input_num_y, input_num_x) > 128:
         print("INPUT INT! ERROR")
         return None
+    from BinaryCalcFunc import binary_format
+
     x_sign_bit = False
     y_sign_bit = False
     if input_num_x < 0:
         x_sign_bit = True
     if input_num_y < 0:
         y_sign_bit = True
-
+    input_num_x = abs(input_num_x)
+    input_num_y = abs(input_num_y)
     binary_len_set = 16
 
     # print(math.floor(input_num_x))
@@ -34,11 +36,21 @@ def iee_addition(input_num_x, input_num_y):
     # print(f"mantissa_y_part = {mantissa_y_binary}")
 
     one_hundred_twenty_seven_binary = binary_format(127, 8)
+
     binary_x_exponent = binary_format(len(binary_x_exponent),8)
     binary_y_exponent = binary_format(len(binary_y_exponent),8)
 
-    binary_x_exponent = binary_array_sum([binary_x_exponent, one_hundred_twenty_seven_binary], True, True)
-    binary_y_exponent = binary_array_sum([binary_y_exponent, one_hundred_twenty_seven_binary], True, True)
+    from BinaryCalcFunc import binary_array_sum
+    # from BinaryCalcFunc import binary_subtraction
+
+    if x_sign_bit == 0 or x_sign_bit == 1:
+        binary_x_exponent = binary_array_sum([binary_x_exponent, one_hundred_twenty_seven_binary], True, True)
+    #else:
+    #    binary_x_exponent = binary_subtraction(one_hundred_twenty_seven_binary, binary_x_exponent)
+    if y_sign_bit == 0 or y_sign_bit == 1:
+        binary_y_exponent = binary_array_sum([binary_y_exponent, one_hundred_twenty_seven_binary], True, True)
+    #else:
+    #    binary_y_exponent = binary_subtraction(one_hundred_twenty_seven_binary, binary_y_exponent)
 
     float_len_x = str(input_num_x)
     float_len_x = float_len_x.split('.')
@@ -50,9 +62,6 @@ def iee_addition(input_num_x, input_num_y):
 
 
     round_arrange = (max(len(float_len_x[1]),len(float_len_y[1])))
-    # mantissa_x_binary = mantissa_x_part
-    # mantissa_y_binary = mantissa_y_part
-
     #print(round(mantissa_x,round_arrange))
     # fraction bits, mantissa
     for i in range(1,24 - len(mantissa_x_binary)):
@@ -73,12 +82,18 @@ def iee_addition(input_num_x, input_num_y):
     # print(f"input_num_Y = {input_num_y}")
     # print(f"binary_x_exponent = {binary_x_exponent}, mantissa x = {mantissa_x_binary}")
     # print(f"binary_y_exponent = {binary_y_exponent}, mantissa y = {mantissa_y_binary}")
+    status_x = ''
+    status_y = ''
+    if x_sign_bit == 1:
+        status_x = '-'
+    if y_sign_bit == 1:
+        status_y = '-'
 
-    print(f"full {input_num_x} representation: {int(x_sign_bit)}.{binary_x_exponent}.{mantissa_x_binary}")
-    print(f"full {input_num_y} representation: {int(y_sign_bit)}.{binary_y_exponent}.{mantissa_y_binary}")
+    print(f"full {status_x}{input_num_x} representation: {int(x_sign_bit)}.{binary_x_exponent}.{mantissa_x_binary}")
+    print(f"full {status_y}{input_num_y} representation: {int(y_sign_bit)}.{binary_y_exponent}.{mantissa_y_binary}")
 
 
-iee_addition(54.4,85.125) #54.4
+iee_addition(-54.4,85.125) #54.4
 
 # 54.4
 # 0 10000100    10110011001100110011010
