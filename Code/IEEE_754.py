d3f696dbffe32519444f70880e789114e63df219
Serjo <savenkoserhii@knu.ua>
2023-03-28 14:33:26 +0300

    beta 0.4.9 - tested a bit, works just fine.

--- Code/IEEE_754.py
+++ Code/IEEE_754.py
@@ -88,6 +88,8 @@ def iee_addition(input_num_x, input_num_y):
 
 
     if input_num_x >= input_num_y:
+        number_1 = max(input_num_x, input_num_y)
+        number_2 = min(input_num_x, input_num_y)
         Exponent_number_1 = Exponent_x - 127
         Exponent_number_2 = Exponent_y - 127
     else:
@@ -101,7 +103,7 @@ def iee_addition(input_num_x, input_num_y):
         max_check = max(Exponent_number_2, Exponent_number_1)
         if check == Exponent_number_1:
             # x is smaller (have smaller exponent check)
-            bigger_mantissa = mantissa_y_binary[0:max_check]
+            bigger_mantissa = mantissa_y_binary[0:max_check + 2]
             bigger_exponent = binary_y_exponent
             bigger_num_sing = y_sign_bit
             bigger_mantissa_start = dot_number_y
@@ -119,19 +121,32 @@ def iee_addition(input_num_x, input_num_y):
                 prolonged_smaller_mantissa_part = bigger_mantissa_start + mantissa_y_binary[0:Exponent_number_2 + 2]
             else:
                 prolonged_smaller_mantissa_part =  mantissa_y_binary[0:Exponent_number_2 + 2]
-
         prolonged_smaller_mantissa_part = prolonged_smaller_mantissa_part.zfill(max_check)
         number_from_mantissas_sum = binary_array_sum([prolonged_smaller_mantissa_part, bigger_mantissa], True, True)
         print(f"prolonged smaller exponent mantissa = {prolonged_smaller_mantissa_part}, bigger exponent mantissa part = {bigger_mantissa}, their summ = {number_from_mantissas_sum}")
-        new_sum_exponent = binary_array_sum([bigger_exponent, one_hundred_twenty_seven_binary], True, True)
+        # new_sum_exponent = binary_array_sum([bigger_exponent, one_hundred_twenty_seven_binary], True, True)
         # new_ieee_754_number = str(int(bigger_num_sing)) + "." + bigger_exponent + "." + number_from_mantissas_sum[::-1].zfill(23)[::-1] # 01000010110100110000000000000000
         new_ieee_754_number = str(int(bigger_num_sing)) + bigger_exponent + number_from_mantissas_sum[::-1].zfill(23)[::-1]
 
         print(f"sum = {new_ieee_754_number}")
+    else:
+        number_1_mantissa_part = mantissa_x_binary[0:Exponent_number_1 + 2]
+        number_2_mantissa_part = mantissa_y_binary[0:Exponent_number_2 + 2]
+        # bigger_num_sing = x_sign_bit
+        bigger_num_sing = y_sign_bit
+        if binary_x_exponent == binary_y_exponent:
+            binary_x_exponent = binary_array_sum([binary_x_exponent, "1"], True, True)
+        number_from_mantissas_sum = binary_array_sum([number_1_mantissa_part, number_2_mantissa_part], True, True)
+        print(
+            f"number 1 mantissa part = {number_1_mantissa_part}, number 2 mantissa part = {number_2_mantissa_part}, their summ = {number_from_mantissas_sum}")
+        new_ieee_754_number = str(int(bigger_num_sing)) + binary_x_exponent + number_from_mantissas_sum[::-1].zfill(23)[
+                                                                            ::-1]
+        print(f"sum = {new_ieee_754_number}")
 
-iee_addition(35.75,20.5) # 54.4, 12.125 I received 13.25 not 66.525 - error somewhere in mantissa migration (notes) 12.125,54.4,
-
-#iee_addition(54.4, 12.125)
+#iee_addition(35.75,20.5) # 56.25 = 01000010011000010000000000000000
+# iee_addition(12.5, 15.8) # 28.3 = 01000001111000100000000000000000
+# iee_addition(34.4, 20.125) # 54.525 = 01000010010110010000000000000000
+# iee_addition(46.6, 12.1) # 58.7 = 01000010011010100000000000000000
 # 54.4
 # 0 10000100    10110011001100110011010
 # 0 10000100    10110011001100110011010
